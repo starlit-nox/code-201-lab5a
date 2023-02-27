832af44daf64f9cadaba2950d1993072418ad417
starlit-nox <kdkelly.1998@gmail.com>
2023-02-27 16:09:41 -0600

    q5

--- js/app.js
+++ js/app.js
@@ -103,9 +103,6 @@ function sumArray(sumArr) { //eslint-disable-line
         sum = sum + sumArr[i];
     }
 
-
-
-
     let ans = [sum, sumArr[0] + "," + sumArr[1] + "," + sumArr[2] + " was passed in as an array of numbers, and " + sum + " is their sum."];
     console.log(ans)
     return ans;
@@ -113,7 +110,7 @@ function sumArray(sumArr) { //eslint-disable-line
 
 // Here is the test for sumArray(); uncomment it to run it
 
-testSumArray(testArray);
+// testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -128,12 +125,24 @@ IMPORTANT DETAIL: You may not use the arithmetic operator * in this function. To
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiplyArray() function and see if the test passes.*/
 
 // Write your code here
-function multiplyArray(multArr) { //eslint-disable-line
+let multiTest = [2,3,4]
+
+function multiplyArray (multiArr) { //eslint-disable-line
+    let product = multiArr[0];
+
+    for (let i = 1; i < multiArr.length; i++) {
+        product = product * multiArr[i];
+    }
+
+    // "The numbers 2,3,4 have a product of 24."
+    let ans = [product, "The numbers " + multiArr[0] + "," + multiArr[1] + "," + multiArr[2] + " have a product of " + product + "."];
+    console.log(ans)
+    return ans;
 
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+testMultiplyArray(multiTest);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 
